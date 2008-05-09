ac7d12fd16380fded05f21176a48c9ed0dcd4824
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-05-09 16:21:17 +0000

    allow

--- slc/subsite/__init__.py
+++ slc/subsite/__init__.py
@@ -1,3 +1,12 @@
+from AccessControl import ModuleSecurityInfo
+from slc.subsite.interfaces import ISubsiteEnhanced
+
+ModuleSecurityInfo('slc.subsite').declarePublic('isSubsite')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
+
+def isSubsite(ob):
+    """ convenience method for skin scripts """
+    return ISubsiteEnhanced.providedBy(ob)    
