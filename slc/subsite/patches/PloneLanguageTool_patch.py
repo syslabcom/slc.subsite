d595767167dd33392b179b853603073d08aedda6
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-09-23 10:42:42 +0000

    added missing patches dir

---
+++ slc/subsite/patches/PloneLanguageTool_patch.py
@@ -0,0 +1,38 @@
+from Products.CMFCore.utils import getToolByName
+from slc.subsite import isSubsite
+from Acquisition import aq_parent
+from Products.PloneLanguageTool.LanguageTool import LanguageTool
+
+def setDefaultLanguage(self, langCode):
+    """Sets the default language."""
+#    import pdb; pdb.set_trace()
+    if isSubsite(aq_parent(self)):
+        return aq_parent(self).getField('default_language').getMutator(aq_parent(self))(langCode)
+    portal_properties = getToolByName(self, "portal_properties")
+    site_properties = portal_properties.site_properties
+    if site_properties.hasProperty('default_language'):
+        return site_properties._updateProperty('default_language', langCode)
+    portal = getUtility(ISiteRoot)
+    if portal.hasProperty('default_language'):
+        return portal._updateProperty('default_language', langCode)
+    self.default_lang = langCode
+
+LanguageTool.setDefaultLanguage = setDefaultLanguage
+
+def getDefaultLanguage(self):
+    """Returns the default language."""
+#    import pdb; pdb.set_trace()
+    if isSubsite(aq_parent(self)):
+        return aq_parent(self).getField('default_language').getAccessor(aq_parent(self))()
+    portal_properties = getToolByName(self, "portal_properties", None)
+    if portal_properties is None:
+        return 'en'
+    site_properties = portal_properties.site_properties
+    if site_properties.hasProperty('default_language'):
+        return site_properties.getProperty('default_language')
+    portal = getUtility(ISiteRoot)
+    if portal.hasProperty('default_language'):
+        return portal.getProperty('default_language')
+    return getattr(self, 'default_lang', 'en')
+
+LanguageTool.getDefaultLanguage = getDefaultLanguage
---
+++ slc/subsite/patches/__init__.py
@@ -0,0 +1,1 @@
+import PloneLanguageTool_patch
