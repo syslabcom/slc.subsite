0670642591c9bf92fc3621067af60e484c610dc4
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-03-03 16:10:31 +0000

    when context is the FactoryTool, we need a special way of determining the parent object; potentially ugly, but it works...

--- slc/subsite/root.py
+++ slc/subsite/root.py
@@ -1,9 +1,10 @@
-from Acquisition import aq_base
+from Acquisition import aq_base, aq_parent
 
 from slc.subsite.interfaces import ISubsiteEnhanced
 
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import utils
+from Products.CMFPlone.interfaces import IFactoryTool
 
 def getSubsiteRoot(context, relativeRoot=None):
     """Get the path to the root of the navigation tree. If context or one of
@@ -25,7 +26,13 @@ def getSubsiteRoot(context, relativeRoot=None):
     portal = portal_url.getPortalObject()
     obj = context
     while not ISubsiteEnhanced.providedBy(obj) and aq_base(obj) is not aq_base(portal):
-        obj = utils.parent(obj)
+        # XXX a bit ugly: if this method is called with a context from inside the portal_factory, we cannot
+        # use the parent() method, as it calls aq_parent on aq_inner of the object. This directly returns the main
+        # portal
+        if IFactoryTool.providedBy(obj):
+            obj = aq_parent(obj)
+        else:
+            obj = utils.parent(obj)
     if ISubsiteEnhanced.providedBy(obj) and aq_base(obj) is not aq_base(portal):
         return '/'.join(obj.getPhysicalPath())
 
