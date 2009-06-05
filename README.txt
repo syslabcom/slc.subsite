5d9f1b5f6631801053607acc7c60d25c95220b41
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-06-05 15:42:03 +0000

    preparing release

--- README.txt
+++ README.txt
@@ -1,7 +1,16 @@
-slc.subsite Package Readme
-=========================
+.. contents::
 
-Overview
---------
+.. Note!
+   -----
+   
+   - code repository
+   - bug tracker
+   - questions/comments feedback mail
 
-Make a Folder behave like a Subsite
+
+- Code repository: https://svn.syslab.com/svn/syslabcom/slc.subsite/
+- Questions and comments to info (at) syslab (dot) com
+- Report bugs at http://products.syslab.com/products/slc.subsite/issues
+
+
+
--- setup.py
+++ setup.py
@@ -1,12 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the slc.subsite package
+"""
+import os
 from setuptools import setup, find_packages
 
-version = '0.1'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('slc', 'subsite', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' 
+    )
+    
+tests_require=['zope.testing']
 
 setup(name='slc.subsite',
       version=version,
       description="Make a Folder behave like a Subsite",
-      long_description="""\
-""",
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
@@ -18,17 +47,22 @@ setup(name='slc.subsite',
       keywords='subsite skin',
       author='Syslab.com GmbH',
       author_email='info@syslab.com',
-      url='http://svn.plone.org/svn/collective/slc.subsite',
+      url='https://svn.syslab.com/svn/syslabcom/slc.subsite/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['slc'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
+          'setuptools',    
           # -*- Extra requirements: -*-
       ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'slc.subsite.tests.test_docs.test_suite',
       entry_points="""
-      # -*- Entry points: -*-
+      # -*- entry_points -*- 
       """,
-      )
+      paster_plugins = ["ZopeSkel"],
+      )      
+      
